d768fc237f0d11e930ba1beed6b336df5c72f94b
Shane A. Stillwell <shanestillwell@gmail.com>
2016-04-16 16:15:32 -0500

    Video 4.3 Connecting to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,19 @@
 
 const express = require('express')
 const compression = require('compression')
+const mongoose = require('mongoose')
+const Bluebird = require('bluebird')
 
 const app = express()
 const api = require('./api')
+const config = require('./config')
+
+// Connect to mongo
+mongoose.Promise = Bluebird
+if (!mongoose.connection.db) mongoose.connect(config.get('DB_URI'))
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', console.log.bind(console, 'connected to mongodb'))
 
 app.use(compression())
 
